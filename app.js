11e42594a96528658a9f15f7ab0f24c854a050a0
AhaetullaHL <hermann.ledent@gmail.com>
2020-10-14 17:20:30 +0200

    WIP api's ajax request

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-    let meteo = {};
+    const cityInput = document.querySelector('#city');
+    const appForm = document.querySelector('.app-form');
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Bordeaux&appid=26ef2197205b6d826ebba06de95ad328')
+    appForm.addEventListener('submit', function (e){
+        e.preventDefault();
+        let city = cityInput.value;
+        console.log(city);
+        weather(city);
+    })
+
+
+})
+
+function weather(city)
+{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=26ef2197205b6d826ebba06de95ad328&units=metric`)
         .then(data => data.json())
         .then(results => {
             console.log(results);
-            meteo = results.list;
+            let meteo = results.list;
             console.log(meteo);
         })
-        .catch(err => console.error('Failed request.'))
-        .finally()
-})
+        .catch(err => console.error('Failed request.' + err))
+
+
+}
